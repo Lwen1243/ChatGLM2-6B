1162a5eaae7b4eb3bc35e949ca3aea099d95d3b5
Lwen1243 <92636994+Lwen1243@users.noreply.github.com>
2023-12-19 12:16:16 +0000

    second

--- web_demo2.py
+++ web_demo2.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="ChatGLM2-6b 演示",
+    page_title="医疗问答系统",
     page_icon=":robot:",
     layout='wide'
 )
@@ -11,8 +11,8 @@ st.set_page_config(
 
 @st.cache_resource
 def get_model():
-    tokenizer = AutoTokenizer.from_pretrained("chatglm2-6b", trust_remote_code=True)
-    model = AutoModel.from_pretrained("chatglm2-6b", trust_remote_code=True).cuda()
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
     # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
@@ -22,7 +22,7 @@ def get_model():
     
 tokenizer, model = get_model()
 
-st.title("ChatGLM2-6B")
+st.title("你有什么地方不舒服吗？")
 
 if 'history' not in st.session_state:
     st.session_state.history = []
@@ -40,9 +40,9 @@ with st.chat_message(name="user", avatar="user"):
 with st.chat_message(name="assistant", avatar="assistant"):
     message_placeholder = st.empty()
 
-prompt_text = st.text_area(label="用户命令输入",
+prompt_text = st.text_area(label="描述",
                            height=100,
-                           placeholder="请在这儿输入您的命令")
+                           placeholder="请在这儿输入您的描述")
 
 button = st.button("发送", key="predict")
 
